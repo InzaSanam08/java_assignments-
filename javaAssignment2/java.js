4d1c3b07fe2a348ed9a46eb660f6a8986feb0a06
inzasanam08 <inzasanam7@gmail.com>
2025-06-04 15:31:20 +0500

    java_assignment

---
+++ javaAssignment2/java.js
@@ -0,0 +1,58 @@
+// VARIABLES FOR STRINGS
+
+
+// 1. Declare a variable called username.
+var username;
+
+// 2. Declare a variable called myName & assign to it a string
+// that represents your Full Name.
+var myName; 
+myname ="Inza Sanam";
+
+
+// 3. Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+var massage = "Hello World!";
+alert(massage)
+
+
+// 4. Write a script to save student’s bio data in JS variables and
+// show the data in alert boxes.
+alert("Inza Sanam")
+alert("Age : 16")
+alert("Certified Mobile Application development ")
+
+
+
+// 5. Write a script to display the following alert using one JS
+// variable:
+alert("PIZZA \n PIZZ \n PIZ \n PI \n P")
+
+// 6. Declare a variable called email and assign to it a string that
+// represents your Email Address(e.g. example@example.com).
+// Show the blow mentioned message in an alert box.(Hint: use
+// string concatenation)
+
+var email = "inzasanam7@gmail.com";
+
+alert(email)
+
+// 7. Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box:
+
+var book = "A smarter way to learn JavaScript";
+alert(book)
+
+
+// 8. Write a script to display this in browser through JS
+document.write("<h1> Hello! This is displayed in the browser.</h1>");
+
+
+// 9. Store following string in a variable and show in alert and
+// browser through JS
+
+var design = " “▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬” ";
+alert(design)
---
+++ javaAssignment5/java.js
@@ -0,0 +1,142 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+// VARIABLE NAMES: LEGAL & ILLEGAL
+
+
+
+// 1. Declare 3 variables in one statement.
+var name;
+
+// 2. Declare 5 legal & 5 illegal variable names.
+// ✅5 legal variable names:
+
+var firstName;
+var _userName;
+var $amount;
+var age2;
+var camelCaseVar;
+
+// ❌ 5 illegal variable names:
+
+
+// var 2name;         // ❌ Starts with a number
+// var user-name;     // ❌ Hyphen is not allowed
+// var var;           // ❌ "var" is a reserved keyword
+// var alert();       // ❌ Can't declare a variable like a function
+// var first name;    // ❌ No spaces allowed in variable names
+
+// 3. Display this in your browser
+// a) A heading stating “Rules for naming JS variables”
+// b) Variable names can only contain ______, ______,
+// ______ and ______.
+// For example $my_1stVariable
+// c) Variables must begin with a ______, ______ or
+// _____. For example $name, _name or name
+// d) Variable names are case _________
+// e) Variable names should not be JS _________
+
+
+
+document.write(" <h1>“Rules for naming JS variables”</h1> ");
+
+document.write(" Variable names can only contain, numbers, $ and. For example: Smy_1stVariable ");
+document.write(" Variable must begin with a letter, $or. For example: Sname,_name or name ");
+document.write(" Variable names are case sensitive  ");
+document.write(" Variable names should not be JS keywords ");
+
+
+// document.write(" <h1>“Rules for naming JS variables”</h1> \n  Variable names can only contain, numbers, $ and. For example: Smy_1stVariable \n Variable must begin with a l
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// MATH EXPRESSIONS
+
+ // 1. Write a program to take a number in a variable, do the
+// required arithmetic to display the following result in your
+// browser:
+ var a = 10;
+    document.write("The value of a is: " + a + "<br><br>");
+
+    document.write("The value of ++a is: " + ++a + "<br>");
+    document.write("Now the value of a is: " + a + "<br><br>");
+
+    document.write("The value of a++ is: " + a++ + "<br>");
+    document.write("Now the value of a is: " + a + "<br><br>");
+
+    document.write("The value of --a is: " + --a + "<br>");
+    document.write("Now the value of a is: " + a + "<br><br>");
+
+    document.write("The value of a-- is: " + a-- + "<br>");
+    document.write("Now the value of a is: " + a + "<br>");
+
+
+// 2. What will be the output in variables a, b & result after
+// execution of the following script:
+// var a = 2,b = 1;
+// var result = --a - --b + ++b + b--;
+//                1 - 0  +  0  +  0   
+// Explain the output at each stage:
+// --a;
+// --a - --b;
+// --a - --b + ++b;
+// --a - --b + ++b + b--; 
+
+
+
+
+
+// 3. Write a program that takes input a name from user &
+// greet the user.
+
+
+var userInPut = prompt("Enter your Name");
+alert("Hello!" + userInPut + "Weelcome!!...")
+
+
+// 5. Write a program to take input a number from user &
+// display it’s multiplication table on your browser. If user
+// does not enter a new number, multiplication table of 5
+// should be displayed by default.
+// 6. Take
+// a) Take three subjects name from user and store them in 3
+// different variables.
+// b) Total marks for each subject is 100, store it in another
+// variable.
+// c) Take obtained marks for first subject from user and
+// stored it in different variable.
+// d) Take obtained marks for remaining 2 subjects from user
+// and store them in variables.
+// e) Now calculate total marks and percentage and show the
+// result in browser like this.(Hint: user tab
+
+
+
+

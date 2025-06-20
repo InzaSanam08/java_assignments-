663ac9204ff4eebd7dfcaeb9405875aaaa8b831d
InzaSanam08 <inzasanam7@gmail.com>
2025-06-20 03:52:33 -0700

    Assignment Added

--- assignment9-11/java.js
+++ assignment9-11/java.js
@@ -82,7 +82,6 @@ if (fuel < 0.25) {
     alert("Please refill the fuel in your car");
 }
 
-
 // 5. Run this script, & check whether alert message would be
 // displayed or not. Record the outputs.
 
@@ -90,25 +89,29 @@ if (fuel < 0.25) {
 var a = 4;
 if (++a === 5) {
     alert("given condition for variable a is true");
-  
+    // displayed
 }
 var b = 82;
 if (b++ === 83) {
     alert("given condition for variable b is true");
-  
+    //not displayed
 }
 var c = 12;
 if (c++ === 13) {
-    alert("condition 1 is true");
+    alert("condition 1 is true"); 
+    //not displayed
 }
 if (c === 13) {
     alert("condition 2 is true");
+    //displayed
 }
 if (++c < 14) {
     alert("condition 3 is true");
+    //not displayed
 }
 if (c === 14) {
     alert("condition 4 is true");
+   //displayed
 }
 
 var materialCost = 20000;
@@ -118,15 +121,16 @@ var laborCost = 2000;
 var totalCost = materialCost + laborCost;
 if (totalCost === laborCost + materialCost) {
     alert("The cost equals");
+     //displayed
 }
 if (true) {
     alert("True");
+     //displayed
 }
 if (false) {
     alert("False");
+     // not displayed
 }
-
-
 
 
 
--- javaAssignment6-9/java.js
+++ javaAssignment6-9/java.js
@@ -1,96 +1,105 @@
-// MATH EXPRESSIONS
+// // MATH EXPRESSIONS
 
-// 1. Write a program to take a number in a variable, do the
-// required arithmetic to display the following result in your
-// browser:
-var a = 10;        
-document.write("The value of a is: " + a + "<br><br>");
+// // 1. Write a program to take a number in a variable, do the
+// // required arithmetic to display the following result in your
+// // browser:
+// var a = 10;        
+// document.write("The value of a is: " + a + "<br><br>");
 
-document.write("The value of ++a is: " + ++a + "<br>");
-document.write("Now the value of a is: " + a + "<br><br>");
+// document.write("The value of ++a is: " + ++a + "<br>");
+// document.write("Now the value of a is: " + a + "<br><br>");
 
-document.write("The value of a++ is: " + a++ + "<br>");
-document.write("Now the value of a is: " + a + "<br><br>");
+// document.write("The value of a++ is: " + a++ + "<br>");
+// document.write("Now the value of a is: " + a + "<br><br>");
 
-document.write("The value of --a is: " + --a + "<br>");
-document.write("Now the value of a is: " + a + "<br><br>");
+// document.write("The value of --a is: " + --a + "<br>");
+// document.write("Now the value of a is: " + a + "<br><br>");
 
-document.write("The value of a-- is: " + a-- + "<br>");
-document.write("Now the value of a is: " + a + "<br>");
-
-
-
-// 2. What will be the output in variables a, b & result after
-// execution of the following script:
-// var a = 2, b = 1;
-// var result = --a - --b + ++b + b--;
-// Explain the output at each stage:
-// --a;
-// --a - --b;
-// --a - --b + ++b;
-// --a - --b + ++b + b--;
-
-// 1
-// 1 - 0
-// 1 - 0  + 1
-// 1 - 0  + 1 + 1
+// document.write("The value of a-- is: " + a-- + "<br>");
+// document.write("Now the value of a is: " + a + "<br>");
 
 
 
+// // 2. What will be the output in variables a, b & result after
+// // execution of the following script:
+// // var a = 2, b = 1;
+// // var result = --a - --b + ++b + b--;
+// // Explain the output at each stage:
+// // --a;
+// // --a - --b;
+// // --a - --b + ++b;
+// // --a - --b + ++b + b--;
 
-// 3. Write a program that takes input a name from user &
-// greet the user.
-
-
-var userName = prompt("Enter Your Name");
-
-alert("Aslam u Alaikum" + "" + userName)
-
-
-//5. Write a program to take input a number from user &
-// display it’s multiplication table on your browser. If user
-// does not enter a new number, multiplication table of 5
-// should be displayed by default.
-
-
-var userInPut = prompt("Enter your fav number");
+// // 1
+// // 1 - 0
+// // 1 - 0  + 1
+// // 1 - 0  + 1 + 1
 
 
 
-//6.
-// a) Take three subjects name from user and store them in 3
-// different variables.
-// b) Total marks for each subject is 100, store it in another
-// variable.
-// c) Take obtained marks for first subject from user and
-// stored it in different variable.
-// d) Take obtained marks for remaining 2 subjects from user
-// and store them in variables.
-// e) Now calculate total marks and percentage and show the
-// result in browser like this.(Hint: user table)
 
-var subj1 = prompt("Enter subject Name 1 ");
-var subj2 = prompt("Enter subject Name 2 ");
-var subj3 = prompt("Enter subject Name 3 ");
-
-var totalMarksPerSubject = 100;
-
-var obtainedMark1 = +prompt("Enter " + subj1 + " Marks ");
-var obtainedMark2 = +prompt("Enter " + subj2 + " Marks ");
-var obtainedMark3 = +prompt("Enter " + subj3 + " Marks ");
-
-var totalObtained = subj1 + subj3 + subj3;
-
-var totalMarks = totalMarksPerSubject * 3;
-
-var percentage = (totalObtained / totalMarks) * 100;
+// // 3. Write a program that takes input a name from user &
+// // greet the user.
 
 
-document.write("<h1> Subject Total Marks Obtained Marks Percentage </h1>");
-document.write(subj1 + "100" + obtainedMark1 + percentage + "%");
-document.write(subj2 + totalMarksPerSubject + obtainedMark1 + percentage);
-document.write(subj3 + totalMarksPerSubject + obtainedMark1 + percentage);
+// var userName = prompt("Enter Your Name");
+
+// alert("Aslam u Alaikum" + "" + userName)
+
+
+// //5. Write a program to take input a number from user &
+// // display it’s multiplication table on your browser. If user
+// // does not enter a new number, multiplication table of 5
+// // should be displayed by default.
+
+
+var userInPut = +prompt("Enter your fav number");
+
+if (!userInPut) {
+   userInPut = 5;
+}
+ for (var i = 1; i <= 10; i++) {
+        document.write(userInPut + " x " + i + " = " + (userInPut * i) + "<br>");
+    }
 
 
 
-// /////////////////////////////////////////    Done
+
+
+// //6.
+// // a) Take three subjects name from user and store them in 3
+// // different variables.
+// // b) Total marks for each subject is 100, store it in another
+// // variable.
+// // c) Take obtained marks for first subject from user and
+// // stored it in different variable.
+// // d) Take obtained marks for remaining 2 subjects from user
+// // and store them in variables.
+// // e) Now calculate total marks and percentage and show the
+// // result in browser like this.(Hint: user table)
+
+// var subj1 = prompt("Enter subject Name 1 ");
+// var subj2 = prompt("Enter subject Name 2 ");
+// var subj3 = prompt("Enter subject Name 3 ");
+
+// var totalMarksPerSubject = 100;
+
+// var obtainedMark1 = +prompt("Enter " + subj1 + " Marks ");
+// var obtainedMark2 = +prompt("Enter " + subj2 + " Marks ");
+// var obtainedMark3 = +prompt("Enter " + subj3 + " Marks ");
+
+// var totalObtained = subj1 + subj3 + subj3;
+
+// var totalMarks = totalMarksPerSubject * 3;
+
+// var percentage = (totalObtained / totalMarks) * 100;
+
+
+// document.write("<h1> Subject Total Marks Obtained Marks Percentage </h1>");
+// document.write(subj1 + "100" + obtainedMark1 + percentage + "%");
+// document.write(subj2 + totalMarksPerSubject + obtainedMark1 + percentage);
+// document.write(subj3 + totalMarksPerSubject + obtainedMark1 + percentage);
+
+
+
+// // /////////////////////////////////////////    Done

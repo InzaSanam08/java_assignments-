63e8d2e8640de4eecfebd99b1d184e5a294400a3
Inza Sanam <inzasanam7@gmail.com>
2025-07-07 08:26:07 -0700

    Update app.js

--- assignment21-25/app.js
+++ assignment21-25/app.js
@@ -1,19 +1,15 @@
-// // 1. Write a program that takes two user inputs for first and
-// // last name using prompt and merge them in a new variable
-// // titled fullName. Greet the user using his full name.
-
-const { use } = require("react");
-
-
+ // 1. Write a program that takes two user inputs for first and
+ // last name using prompt and merge them in a new variable
+ // titled fullName. Greet the user using his full name.
 var firstName = prompt("Enter your first name:");
 var lastName = prompt("Enter your last name:");
 
 var fullName = firstName + '' + lastName
 
  alert('Hello ' + fullName + '! Welcome to our website.');
-// // 2. Write a program to take a user input about his favorite
-// // mobile phone model. Find and display the length of user
-// // input in your browser
+ // 2. Write a program to take a user input about his favorite
+// mobile phone model. Find and display the length of user
+// input in your browser
 
 var userPhone = prompt('Enter your Favorite phone');
 

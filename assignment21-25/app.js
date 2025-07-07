be8bb164a497a5f378c22c3015f50353c6216c53
Inza Sanam <inzasanam7@gmail.com>
2025-07-07 08:23:10 -0700

    Update app.js

--- assignment21-25/app.js
+++ assignment21-25/app.js
@@ -10,7 +10,7 @@ var lastName = prompt("Enter your last name:");
 
 var fullName = firstName + '' + lastName
 
-// // alert('Hello ' + fullName + '! Welcome to our website.');
+ alert('Hello ' + fullName + '! Welcome to our website.');
 // // 2. Write a program to take a user input about his favorite
 // // mobile phone model. Find and display the length of user
 // // input in your browser
